913f3dc216279114208aa25c996c70f2059880a7
Gio <giovannirdz76@hotmail.com>
2023-12-06 18:03:02 -0600

    RequestToNulleable

--- Models/Request/RequestENMS.cs
+++ Models/Request/RequestENMS.cs
@@ -9,18 +9,18 @@ namespace ENMService.Models.Request
     public partial class RequestENMS
     {
         public int IdConf { get; set; }
-        public string ConnReadConf { get; set; }
-        public string ConfFrom { get; set; }
-        public string ToConf { get; set; }
-        public string SmtpConf { get; set; }
+        public string? ConnReadConf { get; set; }
+        public string? ConfFrom { get; set; }
+        public string? ToConf { get; set; }
+        public string? SmtpConf { get; set; }
         public long PortConf { get; set; }
-        public string UsernameConf { get; set; }
-        public string PasswordConf { get; set; }
+        public string? UsernameConf { get; set; }
+        public string? PasswordConf { get; set; }
         public int ReadIntervalConf { get; set; }
         public int ResumeIntervalResume { get; set; }
         public DateTime CreationTimeConf { get; set; }
         public DateTime UpdateTimeConf { get; set; }
-        public string ConnInsertConf { get; set; }
-        public string ConnResumeConf { get; set; }
+        public string? ConnInsertConf { get; set; }
+        public string? ConnResumeConf { get; set; }
     }
 }
